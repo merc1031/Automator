8dbc3d484ce3a98d0309cfd04cc0a9ba064c2748
Leonidas Loucas <merc1031@gmail.com>
2014-05-25 05:27:30 -0700

    simplify routes so they can [maybe] be configurable

--- automator/automator/src/automator_web_sup.erl
+++ automator/automator/src/automator_web_sup.erl
@@ -12,8 +12,8 @@ init([]) ->
     error_logger:info_msg("~p:init()~n", [?MODULE]),
     Dispatch = cowboy_router:compile([
                         {'_', [
-                            {"/serial/receiver/vol",receiver_volume_handler, []},
-                            {"/serial/receiver/vol/:cmd/:vol", [{vol, int}, {cmd, function, fun _Atomize(Val) -> {true, binary_to_atom(Val, latin1)} end}], receiver_volume_handler, []}
+%%                            {"/serial/receiver/vol",receiver_volume_handler, []},
+                            {"/serial/receiver/vol/[:cmd/:vol]", [{vol, int}, {cmd, function, fun _Atomize(Val) -> {true, binary_to_atom(Val, latin1)} end}], receiver_volume_handler, []}
                         ]}
                 ]),
     WebListener = ranch:child_spec(automator_web_listener, 5, ranch_tcp, [{port, 9374}, {max_connections, 100}], cowboy_protocol, [{env, [{dispatch, Dispatch}]}]),
