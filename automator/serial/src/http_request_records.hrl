dff7a99ed1a93217a9a32b694b6f441ff9986088
Leonidas Loucas <merc1031@gmail.com>
2014-06-23 03:15:05 -0700

    remove some cruft

--- automator/serial/src/http_request_records.hrl
+++ automator/serial/src/http_request_records.hrl
@@ -14,12 +14,3 @@
           callback :: #partial{},
           start_time :: erlang:timestamp()
 }).
-%%perform_request(Token, #http_queue_element{token=Token, type=Type, url=Url, headers=Headers, body=Body, callback=Callback, start_time=StartTime}=QueueItem, Pid, State) ->
-%%
-%%
-%%Calls to XBMC should look like
-%% Target = {Ip, Port}
-%% CallReq = 
-%%
-%% http_bridge:request_on_queue(xbmc_jsonrpc, get, "http://Ip:Port/jsonrpc", [{"Content-type", "application/json"}], #{ do => TheThing }, FN)
-%% http_bridge:request_on_queue(xbmc_jsonrpc, get, "http://Ip:Port/jsonrpc", [{"Content-type", "application/json"}], #{ do => TheThing }, FN)
